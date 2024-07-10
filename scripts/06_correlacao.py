dd7f6668ecf5510f5bdbef1d9f73e4593c6624bd
felipedantas-pi <tecnologo.felipedantas@gmail.com>
2024-07-10 20:37:44 -0300

    .

--- scripts/06_correlacao.py
+++ scripts/06_correlacao.py
@@ -15,24 +15,120 @@ df['ii_variacao_percentual'].max()
 df["ii_09"].min()
 df["ii_09"].max()
 
+
+################################################
+# Histograma
+################################################
+
 # Gráfico de distribuição univariada usando a estimativa de densidade de kernel
-# https://seaborn.pydata.org/generated/seaborn.kdeplot.html
-sns.kdeplot(data=df, x="ii_variacao_percentual", fill=True) #opção 1
-sns.histplot(data=df, x='ii_variacao_percentual', kde=True) #opção 2
+# Número de bins segundo diferentes regras
+n = len(df)
+sturges_bins = int(np.ceil(np.log2(n) + 1)) # 1ª regra
+rice_bins = int(np.ceil(2 * n**(1/3))) # 2ª regra
+
+# Criando o gráfico Histograma
+hist_plot = sns.histplot(data=df, x='ii_variacao_percentual', kde=True, bins=sturges_bins) #opção 2
+# Renomear os rótulos dos eixos do histograma
+hist_plot.set_xlabel('Percentage Change')
+hist_plot.set_ylabel('Density')
+
+
+#########################################
+## Correlação de Spearman
+########################################
+
+# Selecionando variáveis
+df_2009 = df[["ii_09", "urbanizacao_percentual"]] # Gerará o coeficiente de correlação
+df_2019 = df[["ii_19", "urbanizacao_percentual"]] # Gerará o coeficiente de correlação
+df_matriz = df[["ii_09", "ii_19", "urbanizacao_percentual"]] # Gerará matriz de correlação
+
+# Scipy= Calcula coeficiente de correlação de Spearman com p-value associado
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.spearmanr.html
+# docs:
+# O coeficiente de correlação de ordem de classificação de Spearman é uma medida 
+# não paramétrica da monotonicidade da relação entre dois conjuntos de dados. 
+# Como outros coeficientes de correlação, esse varia entre -1 e +1, com 0 implicando em nenhuma 
+# correlação. 
+# As correlações de -1 ou +1 implicam uma relação monotônica exata. 
+# Correlações positivas implicam que, à medida que x aumenta, y também aumenta. 
+# Correlações negativas implicam que, à medida que x aumenta, y diminui.
+
+def spearmanr_pval(df):
+    """
+    Calcula coeficiente de correlação de Spearman com p-value associado para um DataFrame.
+    
+    Parâmetros:
+    df (DataFrame): DataFrame contendo os dados para calcular a correlação de Spearman.
+    
+    Retorna:
+    rho (DataFrame): Matriz de correlação de Spearman entre as variáveis.
+    pval (DataFrame): Matriz de p-values associados às correlações de Spearman.
+    """
+    rho, pval = stats.spearmanr(df)
+    
+    if df.shape[1] > 2:
+        print("Retornando Matriz de Correlação")
+        rho = pd.DataFrame(rho, index=df.columns, columns=df.columns)
+        pval = pd.DataFrame(pval, index=df.columns, columns=df.columns)
+        return rho, pval
+        
+    print(f"Retornando o coeficiente de correlação.")
+    return rho, pval
+
+# Calcula a matriz/coeficiente de correlação de spearman e os p-valores
+rho09, pval09  = spearmanr_pval(df_2009)
+rho19, pval19  = spearmanr_pval(df_2019)
+rho_df, pval_df  = spearmanr_pval(df_matriz)
+
+#--- Criando o mapa de calor ---#
+custom_labels = ['I.I. 2009','I.I.2019','Urbanized Area (%)']
+
+plt.figure(figsize=(10, 10))
+h = sns.heatmap(
+    data=rho_df, 
+    annot=True, 
+    cmap='coolwarm',
+    vmin=-1, vmax=1)
+
+# Definindo título da figura
+plt.title('Mapa de Calor da Matriz de Correlação de Spearman')
+
+h.set(xlabel="", ylabel="", aspect="equal")
+h.despine(left=True,Bottom=True)
+h.ax.margins(.02)
+for label in h.ax.get_xticklabels
+
+# Ajustando os rótulos do eixo X e Y
+plt.xticks(ticks=range(len(custom_labels)), labels=custom_labels, ha='left')
+plt.yticks(ticks=range(len(custom_labels)), labels=custom_labels)
+
+# Ajusta automaticamente a disposição dos elementos na figura
+plt.tight_layout()
+plt.show()
+
+
+
+###############################################################
+# GRAFICO DE DISPERSÃO + HISTOGRA MARGINAL + CORRELAÇÃO
+###############################################################
+
+# Gráfico de dispersão + histograma marginal para dados do ii_2009
+disp_hist_2009 = sns.jointplot(data=df, x='ii_09', y='urbanizacao_percentual', kind="scatter", height=5, ratio=3 )
+disp_hist_2009.set_axis_labels('Incidence Index', 'Percentage Urbanization')
+# Adicionar anotação da correlação
+plt.annotate(f'Rs: {rho09:.3f}\np-value: {pval09:.3f}', xy=(1, 0.95),
+             xycoords='axes fraction', fontsize=7, ha='right', va='top',
+             bbox=dict(boxstyle="round,pad=0.3", edgecolor='black', facecolor='white'))
+#plt.suptitle(' Índice de Incidência x Urbanização Percentual', y=1.02)
+#plt.show()
 
 
 # Gráfico de dispersão + histograma marginal para dados do ii_2009
-dist2009 = sns.jointplot(data=df, x='ii_09', y='urbanizacao_percentual', kind="scatter", height=5, ratio=3)
-dist2009.set_axis_labels('Índice de Incidência', 'Urbanização Percentual')
+disp_hist_2019 = sns.jointplot(data=df, x='ii_19', y='urbanizacao_percentual', kind="scatter", height=5, ratio=3 )
+disp_hist_2019.set_axis_labels('Incidence Index', 'Percentage Urbanization')
+# Adicionar anotação da correlação
+plt.annotate(f'Rs: {rho19:.3f}\np-value: {pval19:.3f}', xy=(1, 0.95),
+             xycoords='axes fraction', fontsize=7, ha='right', va='top',
+             bbox=dict(boxstyle="round,pad=0.3", edgecolor='black', facecolor='white'))
 #plt.suptitle(' Índice de Incidência x Urbanização Percentual', y=1.02)
 #plt.show()
-
-
-# Gráfico de dispersão + histograma marginal para dados do ii_2019
-dist2019 = sns.jointplot(data=df, x='ii_19', y='urbanizacao_percentual', kind="scatter", height=5, ratio=3)
-dist2019.set_axis_labels('Índice de Incidência', 'Urbanização Percentual')
-#plt.suptitle(' Índice de Incidência x Urbanização Percentual', y=1.02)
-#plt.show()
-
-
-
---
+++ scripts/07_preparando_geodados.py
@@ -0,0 +1,67 @@
+# Dados
+import pandas as pd
+import geopandas as gpd
+
+# Requisições
+import requests
+import zipfile
+import os
+from io import BytesIO
+
+
+
+# Arquivo shapefile dos limites dos estados brasileiros
+url_ibge_brasil = "https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2019/Brasil/BR/br_unidades_da_federacao.zip"
+
+# Nome do arquivo dentro do ZIP que você deseja carregar
+arquivo_vetorial = 'BR_UF_2019.shp'
+
+# Função para baixar e descompactar o arquivo ZIP
+def baixar_e_descompactar(url, diretorio_destino):
+    response = requests.get(url)
+    with zipfile.ZipFile(BytesIO(response.content)) as zip_ref:
+        zip_ref.extractall(diretorio_destino)
+        # Listar todos os arquivos descompactados
+        print("Arquivos no ZIP:")
+        zip_ref.printdir()
+
+# Diretório temporário para armazenar os arquivos descompactados
+diretorio = '../data/ibge/malha_territorial/'
+
+# Criar o diretório temporário se não existir
+os.makedirs(diretorio, exist_ok=True)
+
+# Baixar e descompactar o arquivo ZIP
+baixar_e_descompactar(url_ibge_brasil, diretorio)
+
+# Listar os arquivos descompactados para encontrar o arquivo vetorial
+for root, dirs, files in os.walk(diretorio):
+    for file in files:
+        print(os.path.join(root, file))
+
+# Caminho completo do arquivo vetorial descompactado
+caminho_arquivo_vetorial = os.path.join(diretorio, arquivo_vetorial)
+
+# Carregar o arquivo vetorial com geopandas
+brasil_uf = gpd.read_file(caminho_arquivo_vetorial)
+
+# Carregar arquivo agregado stats
+ii = pd.read_csv('../data/dataset_amostra_stats_media_uf.csv')
+
+brasil_uf['CD_UF'] = brasil_uf['CD_UF'].astype(str)
+ii['coduf'] = ii['coduf'].astype(str)
+
+gdf = brasil_uf.merge(ii, left_on='CD_UF', right_on='coduf')
+
+# Seelcionando colunas desejadas
+gdf = gdf[['CD_UF',
+ 'NM_UF',
+ 'SIGLA_UF',
+ 'NM_REGIAO',
+ 'ii_09',
+ 'ii_19',
+ 'ii_variacao_percentual',
+ 'geometry']]
+
+gdf.to_file('../data/dataset_final.geojson', driver='GeoJSON')
+
